1a76aa49482680636d716342f42e2562a166a132
Gabriel Renato <62205909+grocre@users.noreply.github.com>
2020-07-04 09:36:59 -0300

    Mudei algumas coisas nas rotas

--- user/userController.js
+++ user/userController.js
@@ -1,9 +1,28 @@
 //modulos importados
 const express = require('express')
 const router = express.Router()
+const Catalogo = require("../database/Catalogo")
 
 //rotas
-router.get("/user/me", (req, res) => {
-
+router.get("/user", (req, res) => {
+    res.sendFile()
 })
+
+router.get("/user/catalogo", (req,res) => {
+    res.sendFile()
+})
+
+router.get("/user/catalogo/new", (req, res) => {
+    res.sendFile()
+})
+
+router.post("/user/catalogo/new", (req, res) => {
+    let[name, desc, per, age] = req.body
+    Catalogo.create({
+        
+    })
+})
+
+
+
 module.exports = router
