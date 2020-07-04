127f91a0af031c47ddc349487ace45475122c0bd
Gabriel Renato <62205909+grocre@users.noreply.github.com>
2020-07-04 13:00:38 -0300

    Add files via upload

--- user/userController.js
+++ user/userController.js
@@ -19,7 +19,16 @@ router.get("/user/catalogo/new", (req, res) => {
 router.post("/user/catalogo/new", (req, res) => {
     let[name, desc, per, age] = req.body
     Catalogo.create({
-        
+        name: name,
+        description: desc, 
+        personality: per, 
+        age: age
+    }).then(() => { //checar os status code depois
+        res.redirect("/user/catalogo")
+        res.statusCode = 200
+    }).catch(err => {
+        console.log(err)
+        res.statusCode = 401
     })
 })
 
