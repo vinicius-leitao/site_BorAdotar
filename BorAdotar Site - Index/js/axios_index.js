50ba387442b93fcd8eea9e4d961aac0cf01f9481
Gabriel Renato <62205909+grocre@users.noreply.github.com>
2020-07-06 18:58:12 -0300

    Add files via upload

--- BorAdotar Site - Index/js/axios_index.js
+++ BorAdotar Site - Index/js/axios_index.js
@@ -1,5 +1,23 @@
-axios.get("http://localhost:8000/teste").then(response => {
-    catalogo = response.data
-    console.log(catalogo)
-    lista = document.getElementById("cards")     
-})
+axios.get("http://localhost:8000/exibir").then(response => {
+    let catalogo = response.data
+    let list = document.getElementById("cards")
+    catalogo.forEach(animal => {
+        //criação de elementos pra igualar a o modelo anterior
+        let cards = document.createElement("div")
+        let imagem = document.createElement("a")
+        let cardInfo = document.createElement("div")
+        let nome = document.createElement("a")
+        let regiao = document.createElement("p")
+        
+        nome.innerHTML = animal.nome
+
+        //setagem de atributos para carrgar o estilo do css
+        cards.setAttribute("class", "cards")
+        cardInfo.setAttribute("class", "card-info")
+
+        //configuração de que elementos ficam dentro de quais
+        list.appendChild(cards)
+        cards.appendChild(imagem, cardInfo)
+        cardInfo.appendChild(nome, regiao)
+    })
+})   
