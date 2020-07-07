440f9bad181483182fe5ab5f13bb75febdeb4941
Gabriel Renato <62205909+grocre@users.noreply.github.com>
2020-07-07 17:01:12 -0300

    Arquivo axios da index
    A maior parte tá completa!!! Faltam só alguns detalhes

--- BorAdotar Site - Index/js/axios_index.js
+++ BorAdotar Site - Index/js/axios_index.js
@@ -1,10 +1,10 @@
-axios.get("http://localhost:8000/exibir").then(response => {
+axios.get("http://localhost:8000/exibir_index").then(response => {
     let catalogo = response.data
     let list = document.getElementById("cards")
     catalogo.forEach(animal => {
         //criação de elementos pra igualar a o modelo anterior
         let cards = document.createElement("div")
-        let imagem = document.createElement("a")
+        let link_imagem = document.createElement("a")
         let cardInfo = document.createElement("div")
         let nome = document.createElement("a")
         let regiao = document.createElement("p")
@@ -16,9 +16,13 @@ axios.get("http://localhost:8000/exibir").then(response => {
         cards.setAttribute("class", "cards")
         cardInfo.setAttribute("class", "card-info")
 
+        //setagem dos links
+        nome.setAttribute("href", "/")
+
         //configuração de que elementos ficam dentro de quais
         list.appendChild(cards)
-        cards.appendChild(imagem, cardInfo)
-        cardInfo.appendChild(nome, regiao)
+        cards.appendChild(cardInfo)
+        cardInfo.appendChild(nome)
+        cardInfo.appendChild(regiao)
     })
 })   
