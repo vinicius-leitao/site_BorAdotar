d521cd61683b1b4fa38aea0594dd1151e927d3b3
Gabriel Renato <62205909+grocre@users.noreply.github.com>
2020-07-10 18:01:50 -0300

    Tabela de usuários

---
+++ database/User.js
@@ -0,0 +1,33 @@
+const sequelize = require("sequelize")
+const database = require("./database")
+const connection = require("./database")
+//terminar de fazer
+const User = connection.define('user', {
+    nome: {
+        type: sequelize.STRING,
+        allowNull: false
+    }, sobrenome: {
+        type: sequelize.STRING,
+        allowNull: false
+    }, email: {
+        type: sequelize.TEXT,
+        allowNull: false
+    }, senha: {
+        type: sequelize.TEXT, 
+        allowNull: false
+    }, dataDeNascimento: {
+        type: sequelize.DATE, 
+        allowNull: false
+    }, cep: {
+        type: sequelize.JSON,
+        allowNull: false
+    }, numero: {
+        type: sequelize.NUMBER, 
+        allowNull: false
+    }, complemento: {
+        type: sequelize.TEXT, 
+        allowNull: true
+    }
+})
+
+module.exports = User
