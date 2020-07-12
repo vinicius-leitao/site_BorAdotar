b2acb657eed282671ff8f0bf9c652d0f4985a667
Gabriel Renato <62205909+grocre@users.noreply.github.com>
2020-07-12 18:49:16 -0300

    Controller de Usuários
    Agora com sistema de paginação do catálogo

--- controllers/noAuthUserController.js
+++ controllers/noAuthUserController.js
@@ -4,9 +4,10 @@ const Pet = require("../database/Pet")
 class NonAuthUser{
 
     static homePage(req, res){
-        Pet.findAll({limit: 4}).then(pets => { 
-            res.append("catalog_index", JSON.stringify(pets))
-            res.sendFile(path.resolve(__dirname, "../public/index/index.html"))
+        Pet.findAll({limit: 4}).then(pets => { //exibe 4 itens na index como um mini catalogo/preview
+            res
+                .append("catalog_index", JSON.stringify(pets)) //passando a seleção da tabela como parametro do header
+                .sendFile(path.resolve(__dirname, "../public/index/index.html"))
         })
     }
 
@@ -19,9 +20,30 @@ class NonAuthUser{
     }
 
     static catalogo(req, res){
-        Pet.findAll().then(pets =>{
-            res.append("catalog", JSON.stringify(pets))
-            res.sendFile(path.resolve(__dirname, "../public/catalogo/catalogo.html"))
+        let page = req.params.page
+        let offset
+        if(isNaN(page) || page == 1 || page == undefined){ //se o valor não for um numero ou for igua a 1
+            offset = 0 //o offset indica o primeiro objeto que será exibido
+        } else {
+            offset = (parseInt(page) -1 ) * 20 //caso a página não seja a primeira o offset vai ser o valor da página - 1 x 20
+        }
+        Pet.findAndCountAll({
+            limit: 20, //limita o número de dos exibidos
+            offset: offset,
+            order: [ 
+                ['id', 'ASC'] //usa o id para listar os dados em ordem crescente (ascend)
+            ] 
+        }).then(pets => {
+            let nextPage
+            if(offset + 20 >= pets.count){
+                nextPage = false
+            } else {
+                nextPage = true
+            }
+            res
+                .append("nextPage", nextPage)
+                .append("catalog", JSON.stringify(pets))
+                .sendFile(path.resolve(__dirname, "../public/catalogo/catalogo.html"))
         })  
     }
 }
