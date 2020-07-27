efcdf53468dba54780858dbad99ffc30d8889bd4
Gabriel Renato <62205909+grocre@users.noreply.github.com>
2020-07-27 14:40:47 -0300

    Rotas
    Ainda sem os middlewares

--- routes/rotas.js
+++ routes/rotas.js
@@ -2,10 +2,12 @@ const express = require("express")
 const path = require("path") //ajuda nos caminhos, é mais simples para renderizar as páginas
 const multer = require("multer") //armazenamento de imagens
 const bodyparser = require("body-parser")
+const session = require("express-session")
 
+const User = require("../database/User")
 
-const router = express.Router()
-const upload = multer({dest: 'upload/'})
+const router = express.Router() //inicialização do express
+const upload = multer({dest: './static/upload/'}) //configuração da variável upload do multer
 
 router.use(bodyparser.urlencoded({extended: false}))
 router.use(bodyparser.json())
@@ -21,24 +23,24 @@ const nonAuthUser = require("../controllers/noAuthUserController")
 router.use(express.static("static"))
 
 //rotas
-router.get("/", nonAuthUser.homePage)
 
-router.get("/login", (req, res) => {
-    res.sendFile()
-})
-router.get("/user/me", UserController.userMe) //rota do usuário
+router.get("/", nonAuthUser.homePage) //rota da página príncipal do site 
+router.get("/login", nonAuthUser.login) //rota da página de login
 router.get("/cadastro", nonAuthUser.cadastro) //rota de cadastro
 router.get("/suporte", nonAuthUser.suporte) //rota de suporte
 router.get("/catalogo/:page?", nonAuthUser.catalogo) //rota do catálogo
 router.get("/error", nonAuthUser.error) //rota de erro
+router.get("/user/me", UserController.userMe) //rota do usuário
 router.get("/user/pet/new",PetController.petForm) //passar o middleware
-router.get("/user/chat", UserController.userChat)
+router.get("/user/chat", UserController.userChat) 
 
 //mudar a rota assim que conseguir o html do vinicius
-router.post("/pet", upload.single('petImage'), PetController.create)
+router.post("/pet", upload.single('img'), PetController.create)
 
 //terminar else quando ler a documentação do flash
 router.post("/cadastro", UserController.create)
+
+router.post("/login", UserController.Userlogin)
 
 
 //pré sets de páginas com status code
