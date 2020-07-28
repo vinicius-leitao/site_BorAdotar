e903c6dfabd18c68e6314f3610d7e8b2e13e59d4
João Marcos <joaomarcossleite@gmail.com>
2020-07-28 19:14:03 -0300

    ultimas alteracoes
    reesposividade feita
    
    paginas (por parte do front) praticamente finalizadas

--- Bora Adotar Site - Catálogo/js/script.js
+++ Bora Adotar Site - Catálogo/js/script.js
@@ -6,53 +6,18 @@ const json_cidades = [];
     .then(blob => blob.json())
     .then(data =>  json_cidades.push(data))
 
-
 window.addEventListener('load',function(){
   
-    document.querySelector('#estados').addEventListener('click', function(){
-        MostrarCidade(this.value);
-    }); 
+    MostrarCidade();
   
-    document.querySelector('#login').addEventListener('click', function(){
-
-        MostrarLogin();
-    })
 });
 
 
-function MostrarLogin(e){
-
-    var loginbar = document.querySelector('#loginbar');
-
-    loginbar.classList.remove('display-none');  
-    console.log(e)
-
-    loginbar.addEventListener('click', function(e){
-        
-        if(e.target.id == 'loginbar' || e.target.id == 'close' ){
-            loginbar.classList.add('display-none');
-        }
-    });
-}
-
-
-function MostrarCidade(es){
+function MostrarCidade(){
     var cidadebar = document.querySelector('#cidades');
-    cidadebar.innerHTML = ``;
+    cidadebar.innerHTML = `<option value=""> Cidade </option>`;
       
-    for(var i = 0; i < json_cidades[0].estados.length; i++){
-      if (es == json_cidades[0].estados[i].sigla){
-        cidadebar.innerHTML = `<option value="">Cidade</option>`;
-
-        for(var j = 0; j < json_cidades[0].estados[i].cidades.length; j++){
-            console.log(json_cidades[0].estados[i].cidades[j]);
-            cidadebar.innerHTML += `<option value="">${json_cidades[0].estados[i].cidades[j]}</option>`;
+        for(var j = 0; j < json_cidades[0].estados[18].cidades.length; j++){
+            cidadebar.innerHTML += `<option value="${json_cidades[0].estados[18].sigla}">${json_cidades[0].estados[18].cidades[j]}</option>`;
         }
-        
-      } else if (es == ''){
-        cidadebar.innerHTML = `<select name="txtCidade" id='cidades'>
-        <option value="">Cidade</option>
-        </select>`;
-      } 
-    }
 }
