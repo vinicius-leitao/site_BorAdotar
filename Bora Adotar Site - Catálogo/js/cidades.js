d940ac14195b21571764cf3fc1a4503d7ac6ef76
João Marcos <joaomarcossleite@gmail.com>
2020-07-28 13:33:45 -0300

    up de paginas
    upei a pagina do user e do anuncio do pet

--- Bora Adotar Site - Catálogo/js/cidades.js
+++ Bora Adotar Site - Catálogo/js/cidades.js
@@ -1,39 +1,5 @@
 
-const json_ = 'https://gist.githubusercontent.com/letanure/3012978/raw/2e43be5f86eef95b915c1c804ccc86dc9790a50a/estados-cidades.json';
 
-const json_cidades = [];
-
-fetch(json_).then(blob => blob.json()).then(data => json_cidades.push(data))
-
-window.addEventListener('load',function(){
-  
-    document.querySelector('#estados').addEventListener('click', function(){
-     
-      var cidadebar = document.querySelector('#cidades');
-      cidadebar.innerHTML = ``;
-        
-      for(var i = 0; i < json_cidades.estados.length; i++){
-        if (this.value == json_cidades.estados[i].sigla){
-          cidadebar.innerHTML = `<select name="txtCidade" id='cidades'>
-          <option value="">Cidade</option>`;
-
-          for(var j = 0; j < json_cidades.estados[i].cidades.length; j++){
-              console.log(json_cidades.estados[i].cidades[j]);
-              cidadebar.innerHTML += `<option value="">${json_cidades.estados[i].cidades[j]}</option>`;
-          }
-
-        cidadebar.innerHTML += `</select>`;
-        } else if (this.value == ''){
-
-          cidadebar.innerHTML = `<select name="txtCidade" id='cidades'>
-          <option value="">Cidade</option>
-          </select>`;
-        } 
-      }
-
-    }); 
-  
-});
 
 
 
--- Bora Adotar Site - Catálogo/js/script.js
+++ Bora Adotar Site - Catálogo/js/script.js
@@ -1,7 +1,22 @@
+const json_ = 'https://gist.githubusercontent.com/letanure/3012978/raw/2e43be5f86eef95b915c1c804ccc86dc9790a50a/estados-cidades.json';
+
+const json_cidades = [];
+
+  fetch(json_) 
+    .then(blob => blob.json())
+    .then(data =>  json_cidades.push(data))
+
+
 window.addEventListener('load',function(){
+  
+    document.querySelector('#estados').addEventListener('click', function(){
+        MostrarCidade(this.value);
+    }); 
+  
+    document.querySelector('#login').addEventListener('click', function(){
 
-    document.querySelector('#login').addEventListener('click', MostrarLogin);
-
+        MostrarLogin();
+    })
 });
 
 
@@ -17,8 +32,27 @@ function MostrarLogin(e){
         if(e.target.id == 'loginbar' || e.target.id == 'close' ){
             loginbar.classList.add('display-none');
         }
-        
     });
-   
+}
 
+
+function MostrarCidade(es){
+    var cidadebar = document.querySelector('#cidades');
+    cidadebar.innerHTML = ``;
+      
+    for(var i = 0; i < json_cidades[0].estados.length; i++){
+      if (es == json_cidades[0].estados[i].sigla){
+        cidadebar.innerHTML = `<option value="">Cidade</option>`;
+
+        for(var j = 0; j < json_cidades[0].estados[i].cidades.length; j++){
+            console.log(json_cidades[0].estados[i].cidades[j]);
+            cidadebar.innerHTML += `<option value="">${json_cidades[0].estados[i].cidades[j]}</option>`;
+        }
+        
+      } else if (es == ''){
+        cidadebar.innerHTML = `<select name="txtCidade" id='cidades'>
+        <option value="">Cidade</option>
+        </select>`;
+      } 
+    }
 }
