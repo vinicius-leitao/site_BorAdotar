7baf6b4162120bc70602704fd8f82cf37056871c
Gabriel Renato <62205909+grocre@users.noreply.github.com>
2020-08-18 10:26:23 -0300

    Imagens e autenticação
    Agora carregando imagens dinamicamente da nuvem

--- Bora Adotar Site - Catálogo/js/axios_catalogo.js
+++ Bora Adotar Site - Catálogo/js/axios_catalogo.js
@@ -1,7 +1,24 @@
-axios.get("http://localhost:8000/catalogo").then(response => {
+let search = window.location.search
+axios.get(`http://localhost:3000/catalogo${search}`).then(response => {
     let catalog = JSON.parse(response.headers.catalog)
+    let token = response.headers.cookie
     let list = document.getElementById("catalogo-container")
-    catalog.rows.forEach(animal => {
+    console.log(catalog)
+
+    let cadastro = document.getElementById("cadastro")
+    let login = document.getElementById("login")
+    
+    if(token && token != null && token != "undefined"){
+        //definição
+        login.innerHTML = "Perfil"
+        cadastro.innerHTML = "Chat"
+
+        //setagem de rotas
+        login.setAttribute("href", "/user/me")
+        cadastro.setAttribute("href", "/user/chat")
+    }
+
+    catalog.forEach(animal => {
         //criação de elementos
         let box = document.createElement("article")
         let img = document.createElement("img")
@@ -15,34 +32,25 @@ axios.get("http://localhost:8000/catalogo").then(response => {
 
         //inserção das variáveis nos elementos
         nome.innerHTML = animal.name
+        porte.innerHTML = animal.port.toUpperCase().substr(0,1)
 
-        //setagem de atributos para carregar o estilo do css
+        //setagem de links
+        img.src = animal.url
+
+        //setagem de atributos para carregar o estilo do css e links
         box.classList.add("box-pet")
+        link.setAttribute("href", `http://localhost:3000/pet?id=${animal.id}`)
         regiao.setAttribute("class","regiao-a")
         porte.setAttribute("class", "clear")
         genero.setAttribute("class", "active-border")
 
-        //setagem dos links
-        link.setAttribute("href", "/catalogo")
-
         //configuração de que elementos ficam dentro de quais
+        list.appendChild(box)
         link.appendChild(nome)
         box.appendChild(img)
         box.appendChild(link)
         box.appendChild(regiao)
         box.appendChild(porte)
         box.appendChild(genero)
-        list.appendChild(box)
     })
-
-    let numeracao = 1
-    let secao = document.getElementById("catalogo-container")
-    let menu = document.createElement("ul")
-    while("true" == response.headers.nextPage){
-        let opcoes = document.createElement("li")
-        opcoes.innerHTML == numeracao
-        menu.appendChild(opcoes)
-        numeracao ++
-    }
-    secao.appendChild(menu)
 })
