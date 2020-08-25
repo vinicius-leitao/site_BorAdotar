599a08f4985e0ddc3c73480332d67c4a97fd275e
Gabriel Renato <62205909+grocre@users.noreply.github.com>
2020-08-25 15:44:21 -0300

    Delete socketIO.js

--- chat/socketIO.js
+++
@@ -1,9 +0,0 @@
-const formatMessage = require('./messages')
-const io = require('socket.io')
-
-module.exports = function(socket){
-    socket.on("message", message => {
-        io.emit("chat-message", message)
-    })
-}
-
