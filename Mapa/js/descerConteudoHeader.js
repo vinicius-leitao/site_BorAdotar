140758377a65eb78cbc4a7264c8a46f30a2928c7
Gabriel Renato <62205909+grocre@users.noreply.github.com>
2020-08-27 20:24:58 -0300

    Delete descerConteudoHeader.js

--- Mapa/js/descerConteudoHeader.js
+++
@@ -1,8 +0,0 @@
-(function(){
-   
-    var $span = document.querySelector(".icon-menu3");
-    var $header_bar = document.querySelector(".header-bar");
-    $span.addEventListener("click", function(){
-        $header_bar.classList.toggle("desceHeader");
-    })
-})()
